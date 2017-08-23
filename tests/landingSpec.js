0c32a21235ac3ed54f741f320e56d53fb8c390f5
Monalisa Sahoo <monalisas@yahoo-inc.com>
2017-08-24 00:28:18 +1000

    Create Tests added

--- tests/landingSpec.js
+++ tests/landingSpec.js
@@ -28,28 +28,55 @@ describe.only('CafeTownSend Landing/Private Home Page', () => {
 
 		it('should display the "Hello username" message on successful login ', () => {
 			expect(LandingPage.helloMessage.getText()).to.eventually.equal('Hello ' + TestData.login.validCredentials.userName);
-		})
+		});
 		it('should display the logout button', () => {
 			expect(LandingPage.logoutButton.isDisplayed()).to.eventually.be.true;
-		})
+		});
 
 		it('should display the create button', () => {
 			expect(LandingPage.createButton.isDisplayed()).to.eventually.be.true;
-		})
+		});
 
 		it('should create button be enabled', () => {
 			expect(LandingPage.createButton.isEnabled()).to.eventually.be.true;
 			expect(AssertHelper.assertClass(LandingPage.createButton, 'disabled')).to.eventually.be.false;
 
-		})
+		});
 
 		it('should display the edit button', () => {
 			expect(LandingPage.editButton.isDisplayed()).to.eventually.be.true;
-		})
+		});
 
 		it('should edit button be disabled', () => {
 			expect(AssertHelper.assertClass(LandingPage.editButton, 'disabled')).to.eventually.be.true;
-		})
+		});
+
+		describe('Given the user details are known create a customer', () => {
+			before(() => {
+				LandingPage.createButton.click();
+			});
+
+			it('should display the First name label', () => {
+				expect(LandingPage.firstNameLabel.getText()).to.eventually.equal('First name:');
+			});
+
+			it('should display the Last name label', () => {
+				expect(LandingPage.lastNameLabel.getText()).to.eventually.equal('Last name:');
+			});
+
+			it('should display the Start date label', () => {
+				expect(LandingPage.startDateLabel.getText()).to.eventually.equal('Start date:');
+			});
+
+			it('should display the Email label', () => {
+				expect(LandingPage.emailLabel.getText()).to.eventually.equal('Email:');
+			});
+
+			it('should display Add Button', () => {
+				expect(LandingPage.addButton.isDisplayed()).to.eventually.be.true;
+				expect(LandingPage.addButton.getText()).to.eventually.equal('Add');
+			});
+		});
 	});
 
 });
--- tests/pageObjects/landingPage.js
+++ tests/pageObjects/landingPage.js
@@ -47,6 +47,51 @@ class LandingPage extends BasePage {
 	static get deleteButton() {
 		return element(by.id('bDelete'));
 	}
+
+	/**
+	 * Get the firstName Label
+	 *
+	 * @return {promise}
+	 */
+	static get firstNameLabel() {
+		return element(by.css('body > div > div > div > form > fieldset > label:nth-child(3) > span'));
+	}
+
+	/**
+	 * Get the lastName Label
+	 *
+	 * @return {promise}
+	 */
+	static get lastNameLabel() {
+		return element(by.css('body > div > div > div > form > fieldset > label:nth-child(4) > span'));
+	}
+
+	/**
+	 * Get the StartDate Label
+	 *
+	 * @return {promise}
+	 */
+	static get startDateLabel() {
+		return element(by.css('body > div > div > div > form > fieldset > label:nth-child(5) > span'));
+	}
+
+	/**
+	 * Get the Email Label
+	 *
+	 * @return {promise}
+	 */
+	static get emailLabel() {
+		return element(by.css('body > div > div > div > form > fieldset > label:nth-child(6) > span'));
+	}
+
+	/**
+	 * Get the Add Button
+	 *
+	 * @return {promise}
+	 */
+	static get addButton() {
+		return element(by.css('button[type="submit"]:nth-child(2)'));
+	}
 }
 
 module.exports = LandingPage;
