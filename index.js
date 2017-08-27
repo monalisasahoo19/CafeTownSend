796c915018c574b0a3371b256efc6b1c758dc7fd
Monalisa Sahoo <monalisas@yahoo-inc.com>
2017-08-27 17:21:29 +1000

    some comments

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+
+// You could avoid this by specifying all the test in conf.js like specs: ['tests/specs/*.js']
+// The advantage of having this as separate file are
+//		1.More control of running the scripts in order
+//		2.With global before/beforeEach/after/afterEach you can able to bootstrap ot tear down any resources if any
+
 importTest = (name, path) => {
 	describe(name, () => {
 		require(path);
 	});
 }
+
 
 describe("CafeTownSend Functional Tests", function () {
 	before(function () {
